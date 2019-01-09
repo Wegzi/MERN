c67afa189f298386892e0d9428b852698c10e55e
Lucas Ramos <wegzi.ramos@gmail.com>
2019-01-09 16:54:19 -0200

    delete item

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,35 +1,38 @@
-import uuid from 'uuid'
 import {
   GET_ITEMS,
   ADD_ITEM,
-  DELETE_ITEM
+  DELETE_ITEM,
+  ITEMS_LOADING
 } from '../actions/types';
 
 const initialState = {
-  items: [
-    { id: uuid(), name: 'Eggs' },
-    { id: uuid(), name: 'MIlk' },
-    { id: uuid(), name: 'Steak' },
-    { id: uuid(), name: 'Water' }
-  ]
+  items: [],
+  loading: false
 }
 
 export default function (state = initialState, action) {
   switch (action.type) {
     case GET_ITEMS:
       return {
-        ...state
+        ...state,
+        items: action.payload,
+        loading: false
       };
-      case DELETE_ITEM:
+    case DELETE_ITEM:
       return {
         ...state,
-        items: state.items.filter(item => item.id !== action.payload) // tudo que for diferente do id vai passar
+        items: state.items.filter(item => item._id !== action.payload) // tudo que for diferente do id vai passar
       }
-      case ADD_ITEM:
-        return {
-          ...state,
-          items: [action.payload, ...state.items]
-        }
+    case ADD_ITEM:
+      return {
+        ...state,
+        items: [action.payload, ...state.items]
+      }
+    case ITEMS_LOADING:
+      return{
+        ...state,
+        loading: true
+      }
     default:
       return state;
   }
